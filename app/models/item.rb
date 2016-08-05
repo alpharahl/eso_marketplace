e9e6f7f843c8401b407fdcc17c9599e95ed08673
David Gauntlett <david.m.gauntlett@gmail.com>
2016-08-04 22:55:47 -0400

    Added the ability to add sales to individual items

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,24 @@ class Item < ActiveRecord::Base
 	def getSales
 		prices = Array.new
 		Sale.where("item_id='#{self.id}'").each do |s|
-			prices << [s.price, s.date]
+			prices << {:price => s.price, :date => s.date}
 		end
 		prices
+	end
+
+	def self.get_or_create_item(item_name)
+		item = Item.find_by_name(item_name)
+		if item
+			return item
+		else
+			return Item.new_item_name(item_name)
+		end
+	end
+
+	def self.new_item_name (item_name)
+		item = Item.new
+		item.name = item_name
+		item.save
+		return item
 	end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,12 @@
 class Sale < ActiveRecord::Base
 	belongs_to :Item
-	
+
+	def self.add_sale_to_item(opts)
+		item = Item.get_or_create_item(opts[:item_name])
+		s = Sale.new
+		s.item_id = item.id
+		s.price = opts[:price]
+		s.date = opts[:date] || Time.now
+		s.save
+	end
 end
